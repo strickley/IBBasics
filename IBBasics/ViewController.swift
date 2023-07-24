62a7949d9d80bd2ed84ab3947cd0e334aa2d0d5a
Sarah Strickley <sarahstrickley@cwi.edu>
2023-07-24 13:58:46 -0500

    App is complete with toggle mode.

--- IBBasics/ViewController.swift
+++ IBBasics/ViewController.swift
@@ -10,17 +10,39 @@ import UIKit
 class ViewController: UIViewController {
 
     //MARK: - IB Outlets
+    @IBOutlet weak var onlyLabel: UILabel!
+    @IBOutlet weak var onlyButton: UIButton!
+    
     
     //MARK: - Instance Properties
+ 
+    var appState = true
+    
     
     //MARK: - VDL
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        onlyButton.tintColor = .red
+        onlyButton.setTitle( "👻", for: .normal)
     }
 
     // MARK: - IB Actions
    
+    @IBAction func onlyButtonTapped(_ sender: Any) {
+        //TODO: create conditional to toggle between boolean states
+       appState.toggle()
+        if appState {
+            onlyLabel.text = "Cookies. Cookies. Cookies"
+            view.backgroundColor = .white
+            onlyLabel.backgroundColor = .white
+        } else {
+            //        print("The button was tapped.")
+            onlyLabel.text = "🍪🍪🍪"
+        
+            view.backgroundColor = .green
+            onlyLabel.backgroundColor = .green
+        }
+    }
     
     //MARK: - Instance Methods
     
