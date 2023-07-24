1ee00f7c68f471ac1d66865303296749befe243c
Sarah Strickley <sarahstrickley@cwi.edu>
2023-07-24 11:40:52 -0500

    Initial Commit

---
+++ IBBasics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IBBasics
+//
+//  Created by admin on 7/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
